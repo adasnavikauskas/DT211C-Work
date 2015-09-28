6ac31e8302b8c21177359a3c84f0f9303257603b
Adas Navikauskas <adasnavikauskas@yahoo.co.uk>
2015-09-28 15:10:18 +0000

    made palindrome file

---
+++ lab3/checkpalindrome.py
@@ -0,0 +1,14 @@
+my_string = input("Enter string: ")
+
+my_string = my_string.casefold()
+
+rev_str = reversed(my_string)
+
+if list(my_str) == reversed(my_string):
+{
+  print("it is palindrome")
+}
+else:
+{
+  print("it is not palindrome")
+}
